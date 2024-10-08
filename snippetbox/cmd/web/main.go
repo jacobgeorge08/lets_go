9836c3c4da01bd26f9d132d9866385c388334944
T460 <jacobgeorge08@gmail.com>
2024-10-09 03:44:30 +0530

    learnt how to serve static files with file server

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -7,11 +7,15 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+    fileServer := http.FileServer(http.Dir("./ui/static/"))
+    mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", home)
     mux.HandleFunc("/snippet/view",snippetView)
     mux.HandleFunc("/snippet/create",snippetCreate)
 
-    log.Print("Starting a Server on Port 4000")
+    log.Print("Starting a Server on Port 8000")
     err := http.ListenAndServe(":8000", mux)
     log.Fatal(err)
 }
